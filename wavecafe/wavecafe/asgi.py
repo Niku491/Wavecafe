45e40c294101553d3a9d8209708d0023882fb11c
Niku491 <138996620+Niku491@users.noreply.github.com>
2023-08-08 20:26:32 +0530

    Add files via upload

---
+++ wavecafe/wavecafe/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for wavecafe project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'wavecafe.settings')
+
+application = get_asgi_application()
